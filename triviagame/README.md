869319fdc683900eaeb3ebd90e34d2ec6fe3833f
Ryley Chapman <Ryley20@yahoo.com>
2019-06-11 22:22:35 -0500

    added the functions for the quiz at the end for correct and incorrect answers along with a readme and fixed the links for background display

--- triviagame/README.md
+++ triviagame/README.md
@@ -1,1 +1,2 @@
 # triviagame
+This is a simple trivia quiz for comedic purposes. You have 60 seconds to answer questions of my choosing and at the end it will display correct or incorrect answers.
--- triviagame/assets/javascript/app.js
+++ triviagame/assets/javascript/app.js
@@ -1,6 +1,12 @@
 
         // javascript main quiz middle page
- 
+        var numCorrect = 0;
+        var numIncorrect = 0;
+        var numUnanswered = 0;
+        var quizContainer = document.getElementById("quiz");
+        var resultsContainer = document.getElementById("results");
+        var submitButton = document.getElementById("submit");
+
 var timer = 60;
   function buildQuiz() {
     setInterval(function(){ timerFunction(); console.log("Hello"); }, 1000);
@@ -13,19 +19,37 @@ var timer = 60;
       for (letter in currentQuestion.answers) {
         answers.push(
           `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
+            <input type="radio" id="${questionNumber}-${letter}" name="question${questionNumber}" value="${letter}">
             ${letter} :
             ${currentQuestion.answers[letter]}
           </label>`
         );
       }
       output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
+        `<div class="question" id="question-${questionNumber}"> ${currentQuestion.question} </div>
         <div class="answers"> ${answers.join("")} </div>`
       );
     });
     quizContainer.innerHTML = output.join("");
   }
+
+  var chosenAnswers = [];
+
+  function getQuestionValues(){
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      if(document.getElementById(`${questionNumber}-${currentQuestion.correctAnswer}`).checked){
+        numCorrect+=1;
+      }
+      else{
+        numIncorrect++;
+      }
+    });
+    console.log(chosenAnswers)
+    console.log(numCorrect,"Correct answers");
+  
+  }
+
+
 
   function timerFunction(){
         if(timer>0){   
@@ -40,40 +64,15 @@ var timer = 60;
   
   
   function finishQuiz() {
+      getQuestionValues();
+
       document.getElementById("quizWrapper").style = "display: none;";
       document.getElementById('results').style.display = 'block';
       $("#correctAnswers").text("correct answers: " + numCorrect);
       $("#incorrectAnswers").text("incorrect answers: " + numIncorrect);
       $("#unanswered").text("unanswered: " + numUnanswered);
   }
-  
-  
-  
-  
-  
-  function showResults() {
-    var answerContainers = quizContainer.querySelectorAll(".answers");
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      var answerContainer = answerContainers[questionNumber];
-      var selector = `input[name=question${questionNumber}]:checked`;
-      var userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-      if (userAnswer === currentQuestion.correctAnswer) {
-        numCorrect++;
-        
-
-      }
-    });
-
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  };
-
-  var numCorrect = 0;
-  var numIncorrect = 0;
-  var numUnanswered = 0;
-  var quizContainer = document.getElementById("quiz");
-  var resultsContainer = document.getElementById("results");
-  var submitButton = document.getElementById("submit");
   var myQuestions = [
     {
       question: "What actor plays in The Other Guys?",
